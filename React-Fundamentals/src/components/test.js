d45c6c72057031a3d1c58d43e49f755dc945cfb8
KasunYapa96 <kasunishankayapa1996@gmail.com>
2020-10-08 13:04:18 +0530

    Dokerizing

---
+++ React-Fundamentals/src/components/test.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class test extends Component {
+
+  
+      state = {
+           
+      }
+
+  
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default test;
